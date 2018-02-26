4c5280c2287a7bbe851973cb495e4d2d2923f165
TinyCrocodile <34377785+TinyCrocodile@users.noreply.github.com>
2018-02-26 20:33:04 +0100

    Update AssemblyInfo.vb

--- CourseDrawer/My Project/AssemblyInfo.vb
+++ CourseDrawer/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.2.000.*")>
-<Assembly: AssemblyFileVersion("5.2.000.0")>
+<Assembly: AssemblyVersion("5.3.000.*")>
+<Assembly: AssemblyFileVersion("5.3.000.0")>
