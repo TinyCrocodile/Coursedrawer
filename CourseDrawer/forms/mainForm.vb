322185e64bee8aa758871853d0b320deb1b3658a
TinyCrocodile <tinu.social@outlook.com>
2018-03-19 21:25:46 +0100

    test

--- CourseDrawer/forms/mainForm.vb
+++ CourseDrawer/forms/mainForm.vb
@@ -788,7 +788,7 @@ Public Class mainForm
         'clsCourses.getInstance.SaveXML(root)
         'clsFolders.getInstance.SaveXML(root)
         'lSaveDialog = Nothing
-
+        'Test
     End Sub
 
     Private Sub saveCourse(course As clsCourse)
