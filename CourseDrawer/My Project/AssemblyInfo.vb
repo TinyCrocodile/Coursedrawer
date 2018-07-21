8a54d3a20433b28e6e698f60bfefee94719fdf4d
TinyCrocodile <tinu.social@outlook.com>
2018-07-21 18:17:29 +0200

    Prepare Version 5.3.2

--- CourseDrawer/My Project/AssemblyInfo.vb
+++ CourseDrawer/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.3.000.*")>
-<Assembly: AssemblyFileVersion("5.3.000.0")>
+<Assembly: AssemblyVersion("5.3.2.*")>
+<Assembly: AssemblyFileVersion("5.3.2.0")>
